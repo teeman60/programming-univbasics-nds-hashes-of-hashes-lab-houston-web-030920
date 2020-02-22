2e34dbdabcb82a734207d5d99875800d2ae870af
Taiye Salami <taiye41@gmail.com>
2020-02-22 07:35:38 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -56,7 +56,7 @@ def naming_system
               sub_category: {
                 label: "Species",
                 sub_category: {
-                  label: {}
+                  
                 }
               
               
